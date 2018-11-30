141f25dcd42c73da1c8b16239b7d637c00703703
NicolasGlassey <nicolas.glassey@heig-vd.ch>
2018-11-30 09:32:54 +0100

    Step 01 - Solution

---
+++ TestSaintify/TestBusiness/UnitTestArtist.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace saintify.Business
+{
+    [TestClass]
+    public class UnitTestArtist
+    {
+
+        #region private attributes
+        private Artist artist = null;
+        private String expectedArtistName = "Artist's Name";
+        private String expectedArtistPictureName = "Artist's picture name";
+        private List<Song> expectedArtistListOfSongs = null;
+        #endregion private attributes
+
+        /// <summary>
+        /// This test method initializes variables and objects needed for the next test session.
+        /// Run before each test method.
+        /// </summary>
+        [TestInitialize]
+        public void Init()
+        {
+            this.expectedArtistListOfSongs = new List<Song>();
+            for (int i = 0; i < 10; i++ )
+            {
+                Song song = new Song("Song" + i.ToString(), i+60);
+                this.expectedArtistListOfSongs.Add(song);
+            }
+            artist = new Artist(this.expectedArtistPictureName, this.expectedArtistName,this.expectedArtistListOfSongs);
+        }
+
+        /// <summary>
+        /// This test method is designed to test the Getter in charge to deliver the artist's picutre's name
+        /// </summary>
+        [TestMethod]
+        public void TestMethodGetterActistPictureNameSucess()
+        {
+            //given
+            //please refer to Init() method
+            String actualArtistPictureName = "";
+
+            //then
+            actualArtistPictureName = this.artist.PictureName();
+
+            //when
+            Assert.AreEqual(this.expectedArtistPictureName, actualArtistPictureName);
+        }
+
+        /// <summary>
+        /// This test method is designed to test the Getter in charge to deliver the artist's name
+        /// </summary>
+        [TestMethod]
+        public void TestMethodGetterArtistNameSucess()
+        {
+            //given
+            //please refer to Init() method
+            String actualArtistName = "";
+
+            //then
+            actualArtistName = this.artist.Name();
+
+            //when
+            Assert.AreEqual(this.expectedArtistName, actualArtistName);
+        }
+
+        /// <summary>
+        /// This test method cleanup variables and objects used for the last test session.
+        /// Run after each test method.
+        /// </summary>
+        [TestCleanup]
+        public void Cleanup()
+        {
+            this.artist = null;
+        }
+    }
+}
---
+++ saintify/Business/Artist.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace saintify.Business
+{
+    /// <summary>
+    /// This class is designed to define an artist
+    /// </summary>
+    public class Artist
+    {
+        #region private attributes
+        private String pictureName = "";//artist's picture's file name (see application ressources.resx)
+        private String name = "";//artist's name
+        private List<Song> listOfSongs = null;//list of the artist's songs 
+        #endregion private attributes
+
+        #region constructors
+        /// <summary>
+        /// This constructor initializes a new instance of the artist class.
+        /// </summary>
+        /// <param name="pictureName">The artist's picture (see application ressources.resx</param>
+        /// <param name="name">The artist's name</param>
+        /// <param name="listOfSongs">The list of artist's songs</param>
+        public Artist (String pictureName, String name, List<Song> listOfSongs)
+        {
+            this.pictureName = pictureName;
+            this.name = name;
+            this.listOfSongs = listOfSongs;
+
+        }
+        #endregion constructors
+
+        #region public methods
+        #region getters and setters
+        /// <summary>
+        /// This getter delivers the value of the private attribut "title" 
+        /// </summary>
+        /// <returns>The artist's picture's file name (see application ressources.resx)</returns>
+        public String PictureName()
+        {
+            return this.pictureName;
+        }
+
+        /// <summary>
+        /// This getter delivers the value of the private attribut "name" 
+        /// </summary>
+        /// <returns>The artist's name</returns>
+        public String Name()
+        {
+            return this.name;
+        }
+
+        /// <summary>
+        /// This getter delivers the list of Songs
+        /// </summary>
+        /// <returns>All songs performed by the artists</returns>
+        public List<Song> ListOfSongs()
+        {
+            return this.listOfSongs;
+        }
+        #endregion getters and setters
+        #endregion public methods
+
+
+    }
+}
